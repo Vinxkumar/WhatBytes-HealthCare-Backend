64b5b37a79253ac51bbdad14cedf8d19c3f9d6a2
Vinxkumar <vinxkumar178@gmail.com>
2026-07-04 20:10:41 +0530

    Updated the names

--- healthcare/settings.py
+++ healthcare/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+0b7+9lla3aakhc@jhf_w+#&a9!5@r*kadf7&4o&lgo5y#m)*s'
+SECRET_KEY = 'django-insecure-7^b!649@q(m54p841$u=9nl7(k8&x@q!cq9@=3p--#@z%g81ak'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -30,6 +30,8 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
+AUTH_USER_MODEL = "user.User"
+
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -37,6 +39,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'appointment',
+    'user',
+    'doctor',
+    'patient',
 ]
 
 MIDDLEWARE = [
