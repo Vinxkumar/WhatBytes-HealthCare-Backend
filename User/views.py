b6f3c17f2b51a01597207466e1e3e803008f91d9
Vinxkumar <vinxkumar178@gmail.com>
2026-07-05 14:21:14 +0530

    Added view/responses

--- User/views.py
+++ User/views.py
@@ -1,3 +1,70 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
+from django.contrib.auth import authenticate
+
+from .serializers import (
+    SignupSerializer,
+    LoginSerializer
+)
+
+class SignUpView(APIView):
+    
+    def post(self, request):
+        serializer = SignupSerializer(data = request.data)
+        
+        if serializer.is_valid():
+            user = serializer.save()
+            return Response(
+                {
+                    "message": "User Created Sucessfully",
+                    "user": SignupSerializer(user).data
+                },
+                status=status.HTTP_201_CREATED
+            )
+        return Response(
+            serializer.errors,
+            status=status.HTTP_400_BAD_REQUEST
+        )
+
+class LoginView(APIView):
+    
+    def post(self, request):
+        serializer = LoginSerializer(
+            data=request.data
+        )
+        
+        if not serializer.is_valid():
+            return Response(
+                serializer.errors,
+                status=status.HTTP_404_NOT_FOUND
+            )
+        
+        email = serializer.validated_data["email"]
+        password = serializer.validated_data["password"]
+        
+        user = authenticate(
+            request=request,
+            email = email,
+            password=password
+        )
+        
+        if user is None:
+            return Response(
+                {
+                    "message" : "Invalid Credentials"
+                },
+                status=status.HTTP_401_UNAUTHORIZED
+            )
+        return Response(
+            {
+                "message":"Login Successfull",
+                "user": {
+                    "name": user.name,
+                    "email": user.email
+                }
+            },
+            status=status.HTTP_200_OK
+        )
 # Create your views here.
